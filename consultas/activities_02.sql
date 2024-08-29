79faa3e219470741285f62000fc05bc8893c93c9
Pedro-Agevisa <paulopedro.dev@gmail.com>
2024-08-29 09:28:45 -0400

    corrigido exe n°1 de join's

--- consultas/activities_02.sql
+++ consultas/activities_02.sql
@@ -6,23 +6,25 @@ Considere a base completa, com apenas pedidos entregues
 ---------------------------------------------------
 
 SELECT
-    DISTINCT t1.customer_id AS clientes,
-    SUM(t3.payment_value) AS receita_por_cliente,
-    t2.customer_state
+    t2.customer_state,
+    ROUND(SUM(t3.price), 3) AS receita_total_uf,
+    ROUND(SUM(t3.price) / COUNT(DISTINCT t1.customer_id), 3) 
+    AS avg_receita_cliente
+
 FROM
     tb_orders AS t1
 LEFT JOIN
     tb_customers AS t2 
     ON t1.customer_id = t2.customer_id
 LEFT JOIN
-    tb_order_payments AS t3
+    tb_order_items AS t3
     ON t1.order_id = t3.order_id
 WHERE
     t1.order_status = 'delivered'
 GROUP BY 
-    t1.customer_id
+    t2.customer_state
 ORDER BY
-    SUM(t3.payment_value) DESC
+    SUM(t3.price) / COUNT(DISTINCT t1.customer_id) DESC
 
 ---------------------------------------------------
 
