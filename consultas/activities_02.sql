1384173de12f4cd48051c6f73fa241d64a5aca2c
Pedro-Agevisa <paulopedro.dev@gmail.com>
2024-08-29 10:26:15 -0400

    adicionado .gitignore

--- consultas/activities_02.sql
+++ consultas/activities_02.sql
@@ -71,7 +71,7 @@ FROM
     tb_order_items AS t1
 LEFT JOIN
     tb_sellers AS t2 
-    ON t1.seller_id = t2.seller_id
+    ON t1.seller_id = t2.seller_idgit 
 LEFT JOIN
     tb_orders AS t3
     ON t1.order_id = t3.order_id
